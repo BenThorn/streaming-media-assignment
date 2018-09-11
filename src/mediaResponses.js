150732a4321b202e5537f5343f180818ac742bd1
BenThorn <brt4557@g.rit.edu>
2018-09-10 21:39:43 -0400

    loadFile order fix

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,18 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const getParty = (request, response) => {
-  loadFile(request, response, '../client/party.mp4', 'video/mp4');
-};
-
-const getBling = (request, response) => {
-  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
-};
-
-const getBird = (request, response) => {
-  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
-}
-
 const loadFile = (request, response, filePath, type) => {
   const file = path.resolve(__dirname, filePath);
 
@@ -63,6 +51,19 @@ const loadFile = (request, response, filePath, type) => {
     return stream;
   });
 };
+
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+}
+
 
 module.exports.getParty = getParty;
 module.exports.getBling = getBling;
